67ac6da91de4e6284dcd677298ba33de732ad05f
kaylamckenzie <kmckenzie82@bethelu.edu>
2024-11-11 15:30:47 -0600

    Employee List

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,10 @@
 import { Outlet, Route, Routes } from "react-router-dom";
 import { HomePage } from "./components/homepage/HomePage";
 import { Login } from "./components/Login/Login";
-import { NavBar } from "./components/navbar";
+import { NavBar } from "./components/navbar/navbar";
 import { CreateOrder } from "./components/order_creation/CreateOrder";
 import "./App.css";
+import { EmployeeList } from "./employees/EmployeeList";
 
 export const App = () => {
   return (
@@ -19,6 +20,7 @@ export const App = () => {
       >
         <Route path="home-page" element={<HomePage />} />
         <Route path="create-order" element={<CreateOrder />} />
+        <Route path="employees" element={<EmployeeList />} />
       </Route>
       <Route index element={<Login />} />
     </Routes>
